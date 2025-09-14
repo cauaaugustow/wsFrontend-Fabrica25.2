cc07095e49c78e02f350bbd3cfa9f65d43953b01
Cauã Augusto <162489346+cauaaugustow@users.noreply.github.com>
2025-09-14 03:49:27 -0300

    Quinto commit -- resolvendo bugs

--- wsfrontend-fabrica25.2/src/components/SearchMenu/index.tsx
+++ wsfrontend-fabrica25.2/src/components/SearchMenu/index.tsx
@@ -2,31 +2,25 @@ import '../../app/page'
 import SearchIcon from '../../assets/search.svg'
 import Image from 'next/image'
 import './style.css'
-import { useState, } from 'react'
+
+interface SearchMenuProps {
+  onSearch: (value: string) => void;
+  searchValue: string;
+}
 
 
-
-export function SearchMenu({ onSearch }: { onSearch: (value: string) => void }) {
-  const [searchInput, setSearchInput] = useState("")
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
-    if (searchInput.trim() !== "") {
-      onSearch(searchInput.toLowerCase()) 
-    }
-  }
-
-  return (
-    <section>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text" placeholder="Ex: Pikachu" className="inputSearch" value={searchInput} onChange={(e) => setSearchInput(e.target.value)}
-        />
-        <button className="btnSearch" type="submit">
-          <Image className="searchIcon" src={SearchIcon} alt="Buscar" />
-        </button>
-      </form>
-      <h2 className="title">Lista de Pokémons</h2>
-    </section>
-  )
+export function SearchMenu({ onSearch, searchValue }: SearchMenuProps) {
+    return(
+        <>
+         <section >
+             <form >
+                 <input type="text" placeholder="Ex : Pikachu" value={searchValue} onChange={(e) => onSearch(e.target.value)}/>
+                <button className ='btnSearch' type="submit"  >
+                     <Image className='searchIcon' src={SearchIcon} alt="Buscar"  />
+                 </button>
+             </form>
+             <h2 className='title'>Lista de pokemons</h2>
+         </section>
+         </>
+    )
 }
